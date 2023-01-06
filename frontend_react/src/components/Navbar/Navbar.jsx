9403fa2674d23a83d80222b7d33de683d871899a
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-06 02:46:18 -0500

    styled and designed sidbar and navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt1, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 import "./Navbar.scss";
 import { images } from "../../constants";
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -16,6 +19,40 @@ function Navbar() {
           </li>
         ))}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt1
+          onClick={() => {
+            setToggle(true);
+          }}
+        />
+        {toggle && (
+          <motion.div
+            // whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX
+              onClick={() => {
+                setToggle(false);
+              }}
+            />
+            <ul className="app__navbar-links">
+              {["home", "about", "work", "skills", "contact"].map((item) => (
+                <li key={item}>
+                  {" "}
+                  <a
+                    onClick={() => {
+                      setToggle(false);
+                    }}
+                    href={`#${item}`}
+                  >
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 }
