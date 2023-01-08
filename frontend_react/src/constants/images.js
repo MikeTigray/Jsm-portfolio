a512a40a9bd3255c2d634fc805c1b4ffa30ee499
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-08 01:49:23 -0500

    added profile and styling to header container

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,5 +1,7 @@
 import logo from "../assets/logo.png";
+import profile from "../assets/profile.png";
+import circle from "../assets/circle.svg";
 
-const images = { logo };
+const images = { logo, profile, circle };
 
 export default images;
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,8 +1,47 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 import "./Header.scss";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋🏼</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am </p>
+              <h1 className="head-text">Micheale</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer </p>
+            <p className="p-text"> Free Lancer </p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg " />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay-circle"
+        />
+      </motion.div>
+      <motion.div></motion.div>
+    </div>
+  );
 }
 
 export default Header;
