33c288c5cf5f21d642db6b3f4a8184c470787b4c
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-09 22:31:06 -0500

    set up client file

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+
+import { urlFor, client } from "../../client";
 import "./About.scss";
-const abouts = [
-  {
-    title: "Front-end",
-    description: "I am a good web developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Back-end",
-    description: "I am a good web developer",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a good web developer",
-    imgUrl: images.about03,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Front-end",
+//     description: "I am a good web developer",
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: "Back-end",
+//     description: "I am a good web developer",
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: "UI/UX",
+//     description: "I am a good web developer",
+//     imgUrl: images.about03,
+//   },
+// ];
 function About() {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I know that <span>good design</span>means <span>good business</span>
+        I know that <span>good design</span> means <span>good business</span>
       </h2>
 
-      <div className="app_profiles">
+      <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
@@ -35,7 +45,7 @@ function About() {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
