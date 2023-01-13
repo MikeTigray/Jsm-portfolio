48c81811345d135a2a3c1f7ee8bf6162ebbfb737
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-01-13 00:54:09 -0500

    completed skills section jsx

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactTooltip from "react-tooltip";
 
+import { urlFor, client } from "../../client";
+import { AppWrap } from "../../Wrapper";
+
+import { motion } from "framer-motion";
 import "./Skills.scss";
 function Skills() {
-  return <div>Skills</div>;
+  const [skills, setSkills] = useState([]);
+  const [experience, setExperience] = useState([]);
+
+  useEffect(() => {
+    const exQuery = '*[_type == "experiences"]';
+    const skillQuery = '*[_type == "skills"]';
+
+    client.fetch(exQuery).then((data) => {
+      setExperience(data);
+    });
+    client.fetch(skillQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name}></img>
+              </div>
+              <p className={skill.name}>{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
 }
 
 export default Skills;
